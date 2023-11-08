df8178f0a204f2a3d3deb5b5620a81e19928fef1
Vladi <39379713+vladik93@users.noreply.github.com>
2023-11-08 22:09:19 +0200

    added dynamic change to header on url change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,40 @@
 import "./Header.css";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Header = ({ toggleCart, isCart }) => {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const onCartClick = () => {
-    toggleCart();
-    navigate("/cart");
-  };
+  const [content, setContent] = useState({
+    icon: "cart",
+    title: "Products",
+    toUrl: "/cart",
+  });
 
-  const onCaretClick = () => {
-    toggleCart();
-    navigate(-1);
-  };
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/":
+        setContent({ icon: "cart", title: "Products", toUrl: "/cart" });
+        return;
+      case "/cart":
+        setContent({ icon: "caret_left", title: "Cart", toUrl: "/" });
+        return;
+      case "/payment":
+        setContent({ icon: "cart", title: "Payment", toUrl: "/cart" });
+        return;
+    }
+  }, [location.pathname]);
 
   return (
     <header className="header">
-      {isCart ? (
-        <>
-          <button className="button-icon" onClick={onCaretClick}>
-            <img src="./icons/caret_left.svg" className="icon" />
-          </button>
-          <h3 className="heading-tertiary">My Cart</h3>
-          <button className="button-icon">
-            <img src="./icons/3-bars.svg" className="icon" />
-          </button>
-        </>
-      ) : (
-        <>
-          <button className="button-icon" onClick={onCartClick}>
-            <img src="./icons/cart.svg" className="icon" />
-          </button>
-          <h3 className="heading-tertiary">Store</h3>
-          <button className="button-icon">
-            <img src="./icons/3-bars.svg" className="icon" />
-          </button>
-        </>
-      )}
+      <button className="button-icon" onClick={() => navigate(content.toUrl)}>
+        <img src={`./icons/${content.icon}.svg`} className="icon" />
+      </button>
+      <h3 className="heading-tertiary">{content.title}</h3>
+      <button className="button-icon">
+        <img src="./icons/3-bars.svg" className="icon" />
+      </button>
     </header>
   );
 };
